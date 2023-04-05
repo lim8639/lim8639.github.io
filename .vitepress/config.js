c27638f16924d122ce11afc94820e690aecc6010
lim8639 <863978160@qq.com>
2023-04-05 11:18:27 +0800

    Create static.yml
    Signed-off-by: misaka <56546167+lim8639@users.noreply.github.com>

---
+++ .vitepress/config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vitepress'
+import { pagefindPlugin } from 'vitepress-plugin-pagefind'
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "沫沫的时光机",
+  description: "沫沫的知识库系统",
+  vite:{
+    plugins:[pagefindPlugin()],
+  },
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
